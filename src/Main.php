693ff912f64f735bc8b5cf1454e23c3acc93f9e1
Nerahikada <hkshirorom@yahoo.co.jp>
2022-06-26 10:30:22 +0900

    イベント登録していなかった(ど忘れ)

--- src/Main.php
+++ src/Main.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace NeiroNetwork\BetterPmmp;
 
+use NeiroNetwork\BetterPmmp\modification\DisableComboGlitch;
 use NeiroNetwork\BetterPmmp\modification\RestrictColoredText;
+use NeiroNetwork\BetterPmmp\modification\VanillaLikeChat;
+use NeiroNetwork\BetterPmmp\modification\VanillaLikePlayerList;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
 
 	protected function onEnable() : void{
 		$this->getServer()->getPluginManager()->registerEvents(new RestrictColoredText, $this);
+		$this->getServer()->getPluginManager()->registerEvents(new DisableComboGlitch, $this);
+		$this->getServer()->getPluginManager()->registerEvents(new VanillaLikePlayerList, $this);
+		$this->getServer()->getPluginManager()->registerEvents(new VanillaLikeChat, $this);
 	}
 }
