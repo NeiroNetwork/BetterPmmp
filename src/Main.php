6ff59345d5411256f45d95ff3bf3b3f65d4607a7
Nerahikada <hkshirorom@yahoo.co.jp>
2022-06-27 17:07:31 +0900

    Main: 機能の読み込みを短いコードで実現(?)

--- src/Main.php
+++ src/Main.php
@@ -4,27 +4,16 @@ declare(strict_types=1);
 
 namespace NeiroNetwork\BetterPmmp;
 
-use NeiroNetwork\BetterPmmp\modification\DisableComboGlitch;
-use NeiroNetwork\BetterPmmp\modification\LimitedEnderPearl;
-use NeiroNetwork\BetterPmmp\modification\RestrictColoredText;
-use NeiroNetwork\BetterPmmp\modification\VanillaLikeChat;
-use NeiroNetwork\BetterPmmp\modification\SimplePlayerList;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
 
 	protected function onEnable() : void{
-		$mods = [];
-		$config = $this->getConfig();
-
-		if($config->get("RestrictColoredText")) $mods[] = new RestrictColoredText;
-		if($config->get("DisableComboGlitch")) $mods[] = new DisableComboGlitch;
-		if($config->get("SimplePlayerList")) $mods[] = new SimplePlayerList;
-		if($config->get("VanillaLikeChat")) $mods[] = new VanillaLikeChat;
-		if($config->get("LimitedEnderPearl")) $mods[] = new LimitedEnderPearl;
-
-		foreach($mods as $listener){
-			$this->getServer()->getPluginManager()->registerEvents($listener, $this);
+		foreach($this->getConfig()->getAll() as $key => $value){
+			if($value){
+				$class = "\\NeiroNetwork\\BetterPmmp\\modification\\$key";
+				$this->getServer()->getPluginManager()->registerEvents(new $class, $this);
+			}
 		}
 	}
 }
