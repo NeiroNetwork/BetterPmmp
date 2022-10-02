09fa47db524242bcb0697b7725fa8d27ba94be8c
Nerahikada <hkshirorom@yahoo.co.jp>
2022-10-02 13:18:33 +0900

    `PluginBase::getConfig()` を使わないように(デフォルトの値を設定するのがめんどくさいため) クラスが見つからない場合は、エラーを出力し設定を削除するように

--- src/Main.php
+++ src/Main.php
@@ -5,13 +5,30 @@ declare(strict_types=1);
 namespace NeiroNetwork\BetterPmmp;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use Webmozart\PathUtil\Path;
 
 class Main extends PluginBase{
 
 	protected function onEnable() : void{
-		foreach($this->getConfig()->getAll() as $key => $value){
+		$config = new Config(Path::join($this->getDataFolder(), "config.yml"), default: [
+			"RestrictColoredText" => true,
+			"DisableComboGlitch" => true,
+			"SimplePlayerList" => true,
+			"VanillaLikeChat" => true,
+			"LimitedEnderPearl" => true,
+			"FixFallDamageHeight" => true,
+			"DisableBreakingBySpectator" => true,
+		]);
+
+		foreach($config->getAll() as $key => $value){
 			if($value){
 				$class = "\\NeiroNetwork\\BetterPmmp\\modification\\$key";
+				if(!class_exists($class)){
+					$this->getLogger()->error("Module \"$key\" not found");
+					$config->remove($key);
+					continue;
+				}
 				$this->getServer()->getPluginManager()->registerEvents(new $class, $this);
 			}
 		}
