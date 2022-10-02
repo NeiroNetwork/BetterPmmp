0b6e1492fd822a4bccdac38dae65b67000ffd55b
ch4rindo <103165856+ch4rindo@users.noreply.github.com>
2022-10-02 12:19:46 +0900

    ゲームモードをスペクテイターに変更した際に、ブロックを破壊できないように変更

---
+++ src/modification/ChangeGameModePacketId.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeiroNetwork\BetterPmmp\modification;
+
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketSendEvent;
+use pocketmine\network\mcpe\protocol\SetPlayerGameTypePacket;
+use pocketmine\player\GameMode;
+use pocketmine\player\Player;
+
+class ChangeGameModePacketId implements Listener{
+
+	/**
+	 * @priority LOWEST
+	 */
+	public function onPacketSend(DataPacketSendEvent $event){
+		$pks = $event->getPackets();
+		$sessions = $event->getTargets();
+
+		foreach($pks as $pk){
+			if(!$pk instanceof SetPlayerGameTypePacket) continue;
+			foreach($sessions as $session){
+				$player = $session->getPlayer();
+				if(is_null($player) || !$player instanceof Player) continue;
+				if($player->getGamemode() === GameMode::SPECTATOR()){
+					$pk->gamemode = GameMode::SPECTATOR()->id();
+				}
+			}
+		}
+	}
+}
