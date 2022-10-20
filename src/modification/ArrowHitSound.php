5a75ce0b8f8eb7912d8e6a9d4e8849e7a10782f8
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2022-10-21 00:19:08 +0900

    不要な文を削除

--- src/modification/ArrowHitSound.php
+++ src/modification/ArrowHitSound.php
@@ -4,28 +4,27 @@ declare(strict_types=1);
 
 namespace NeiroNetwork\BetterPmmp\modification;
 
-use pocketmine\entity\Entity;
 use pocketmine\event\entity\EntityDamageByChildEntityEvent;
 use pocketmine\event\Listener;
 use pocketmine\math\Vector3;
 use pocketmine\network\mcpe\protocol\PlaySoundPacket;
 use pocketmine\player\Player;
 
-class ArrowHitSound implements Listener{
+class ArrowHitSound implements Listener {
 
-	public function onEntityDamageByChildEntity(EntityDamageByChildEntityEvent $event){
+	public function onEntityDamageByChildEntity(EntityDamageByChildEntityEvent $event) {
 		$child = $event->getChild();
 		$entity = $event->getEntity();
 		$damager = $event->getDamager();
 
-		if ($child instanceof \pocketmine\entity\projectile\Arrow){
-			if ($damager instanceof Player && $entity instanceof Player){
+		if ($child instanceof \pocketmine\entity\projectile\Arrow) {
+			if ($damager instanceof Player && $entity instanceof Player) {
 				$damager->getNetworkSession()->sendDataPacket($this->sound($damager->getPosition()));
 			}
 		}
 	}
 
-	private function sound(Vector3 $target): PlaySoundPacket{
+	private function sound(Vector3 $target): PlaySoundPacket {
 		return PlaySoundPacket::create("random.orb", $target->x, $target->y, $target->z, 1.0, 0.5);
 	}
 }
