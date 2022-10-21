dd461bb7def16b925ffcf007a7857a7f99148e82
Nerahikada <hkshirorom@yahoo.co.jp>
2022-10-21 15:12:51 +0900

    ArrowHitSound: いくつか変更

--- README.md
+++ README.md
@@ -32,4 +32,6 @@ OP権限を持っていないプレイヤーによる文字の装飾を禁止し
 参照: https://github.com/pmmp/PocketMine-MP/pull/5313
 
 ### ArrowHitSound
-プレイヤーが別のプレイヤーに弓矢を当てた際に、攻撃者に効果音を再生します。
+
+プレイヤーがエンティティに弓矢を当てた際に、攻撃者に効果音を再生します。  
+本来はプレイヤーに当てた場合のみに再生されるのが正しいバニラの挙動です: <https://minecraft.fandom.com/wiki/Bow#Sounds>
--- src/modification/ArrowHitSound.php
+++ src/modification/ArrowHitSound.php
@@ -4,27 +4,28 @@ declare(strict_types=1);
 
 namespace NeiroNetwork\BetterPmmp\modification;
 
+use pocketmine\entity\projectile\Arrow;
 use pocketmine\event\entity\EntityDamageByChildEntityEvent;
 use pocketmine\event\Listener;
 use pocketmine\math\Vector3;
 use pocketmine\network\mcpe\protocol\PlaySoundPacket;
 use pocketmine\player\Player;
 
-class ArrowHitSound implements Listener {
+class ArrowHitSound implements Listener{
 
-	public function onEntityDamageByChildEntity(EntityDamageByChildEntityEvent $event){
-		$child = $event->getChild();
-		$entity = $event->getEntity();
-		$damager = $event->getDamager();
-
-		if ($child instanceof \pocketmine\entity\projectile\Arrow){
-			if ($damager instanceof Player && $entity instanceof Player){
+	/**
+	 * @priority MONITOR
+	 */
+	public function onEntityDamageByChildEntity(EntityDamageByChildEntityEvent $event) : void{
+		if($event->getChild() instanceof Arrow){
+			$damager = $event->getDamager();
+			if($damager instanceof Player){
 				$damager->getNetworkSession()->sendDataPacket($this->sound($damager->getPosition()));
 			}
 		}
 	}
 
-	private function sound(Vector3 $target): PlaySoundPacket{
-		return PlaySoundPacket::create("random.orb", $target->x, $target->y, $target->z, 1.0, 0.5);
+	private function sound(Vector3 $pos) : PlaySoundPacket{
+		return PlaySoundPacket::create("random.orb", $pos->x, $pos->y, $pos->z, 1.0, 1.0);
 	}
 }
