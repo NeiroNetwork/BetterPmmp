624fe8557df9512e5c73bc1aba469eeda2fabc76
Nerahikada <hkshirorom@yahoo.co.jp>
2023-08-26 16:44:24 +0900

    AddEducationItems: コードをより簡潔に

--- src/modification/AddEducationItems.php
+++ src/modification/AddEducationItems.php
@@ -16,32 +16,22 @@ class AddEducationItems extends ModuleBase{
 
 	public function onEnabled() : void{
 		$colors = [DyeColor::WHITE(), DyeColor::LIGHT_GRAY(), DyeColor::GRAY(), DyeColor::BLACK(), DyeColor::BROWN(), DyeColor::RED(), DyeColor::ORANGE(), DyeColor::YELLOW(), DyeColor::LIME(), DyeColor::GREEN(), DyeColor::CYAN(), DyeColor::LIGHT_BLUE(), DyeColor::BLUE(), DyeColor::PURPLE(), DyeColor::MAGENTA(), DyeColor::PINK()];
+		$toItems = fn(array $blocks) => array_map(fn(Block $block) => $block->asItem(), $blocks);
 
-		$hardenedGlassesKey = VanillaBlocks::TINTED_GLASS()->asItem();
-		$hardenedGlasses = [VanillaBlocks::HARDENED_GLASS(), ...array_map(fn(DyeColor $color) => VanillaBlocks::STAINED_HARDENED_GLASS()->setColor($color), $colors)];
-		$hardenedGlasses = array_map(fn(Block $block) => $block->asItem(), $hardenedGlasses);
+		$insertItems = [
+			VanillaBlocks::TINTED_GLASS()->asItem()->getStateId() => $toItems([VanillaBlocks::HARDENED_GLASS(), ...array_map(fn(DyeColor $color) => VanillaBlocks::STAINED_HARDENED_GLASS()->setColor($color), $colors)]),
+			VanillaBlocks::STAINED_GLASS_PANE()->setColor(DyeColor::PINK())->asItem()->getStateId() => $toItems([VanillaBlocks::HARDENED_GLASS_PANE(), ...array_map(fn(DyeColor $color) => VanillaBlocks::STAINED_HARDENED_GLASS_PANE()->setColor($color), $colors)]),
+			VanillaBlocks::SOUL_TORCH()->asItem()->getStateId() => $toItems([VanillaBlocks::RED_TORCH(), VanillaBlocks::GREEN_TORCH(), VanillaBlocks::BLUE_TORCH(), VanillaBlocks::PURPLE_TORCH(), VanillaBlocks::UNDERWATER_TORCH()]),
+			VanillaBlocks::TNT()->asItem()->getStateId() => [VanillaBlocks::TNT()->setWorksUnderwater(true)->asItem()],
+			VanillaItems::SUSPICIOUS_STEW()->getStateId() => array_map(fn(int $i) => StringToItemParser::getInstance()->parse("element_$i"), range(0, 118)),
+		];
 
-		$hardenedGlassPanesKey = VanillaBlocks::STAINED_GLASS_PANE()->setColor(DyeColor::PINK())->asItem();
-		$hardenedGlassPanes = [VanillaBlocks::HARDENED_GLASS_PANE(), ...array_map(fn(DyeColor $color) => VanillaBlocks::STAINED_HARDENED_GLASS_PANE()->setColor($color), $colors)];
-		$hardenedGlassPanes = array_map(fn(Block $block) => $block->asItem(), $hardenedGlassPanes);
-
-		$coloredTorchesKey = VanillaBlocks::SOUL_TORCH()->asItem();
-		$coloredTorches = [VanillaBlocks::RED_TORCH(), VanillaBlocks::GREEN_TORCH(), VanillaBlocks::BLUE_TORCH(), VanillaBlocks::PURPLE_TORCH(), VanillaBlocks::UNDERWATER_TORCH()];
-		$coloredTorches = array_map(fn(Block $block) => $block->asItem(), $coloredTorches);
-
-		$underwaterTntKey = VanillaBlocks::TNT()->asItem();
-		$underwaterTnt = [VanillaBlocks::TNT()->setWorksUnderwater(true)->asItem()];
-
-		$elementZeroItem = VanillaBlocks::ELEMENT_ZERO()->asItem();
-		$remainingElementsCount = 0;
-		$elementsKey = VanillaItems::SUSPICIOUS_STEW();
-		$stringToItemParser = StringToItemParser::getInstance();
-		$elements = array_map(fn(int $i) => $stringToItemParser->parse("element_$i"), range(0, 118));
-
-		// ここからクリエイティブインベントリの再構築
 		$creativeInventory = CreativeInventory::getInstance();
 		$creativeContents = $creativeInventory->getAll();
 		$creativeInventory->clear();
+
+		$elementZeroItem = VanillaBlocks::ELEMENT_ZERO()->asItem();
+		$remainingElementsCount = 0;
 
 		foreach($creativeContents as $item){
 			if($item->equals($elementZeroItem)){
@@ -52,15 +42,7 @@ class AddEducationItems extends ModuleBase{
 			}
 
 			$creativeInventory->add($item);
-
-			array_map(fn(Item $i) => $creativeInventory->add($i), match(true){
-				$hardenedGlassesKey->equals($item) => $hardenedGlasses,
-				$hardenedGlassPanesKey->equals($item) => $hardenedGlassPanes,
-				$coloredTorchesKey->equals($item) => $coloredTorches,
-				$underwaterTntKey->equals($item) => $underwaterTnt,
-				$elementsKey->equals($item) => $elements,
-				default => [],
-			});
+			array_map(fn(Item $i) => $creativeInventory->add($i), $insertItems[$item->getStateId()] ?? []);
 		}
 	}
 }
